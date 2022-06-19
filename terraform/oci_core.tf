6f6cc9a55919def0d4fd546f790ed725cbf81578
Seong Yong-ju <sei40kr@gmail.com>
2022-06-20 01:19:06 +0900

    feat(ansible): obtain & renew the certificate
    Obtain & renew the SSL certificate automatically.

--- terraform/oci_core.tf
+++ terraform/oci_core.tf
@@ -46,6 +46,22 @@ resource "oci_core_network_security_group_security_rule" "http" {
   }
 }
 
+resource "oci_core_network_security_group_security_rule" "https" {
+  direction                 = "INGRESS"
+  network_security_group_id = oci_core_network_security_group.instance.id
+  protocol                  = "6"
+  source                    = "0.0.0.0/0"
+  source_type               = "CIDR_BLOCK"
+  stateless                 = false
+
+  tcp_options {
+    destination_port_range {
+      max = 443
+      min = 443
+    }
+  }
+}
+
 resource "oci_core_instance" "calibre_web" {
   agent_config {
     is_management_disabled = true
